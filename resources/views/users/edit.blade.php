a991c22d0ea02062e4c918184fc657bec70d3e9e
vagrant <vagrant@homestead>
2018-06-16 13:41:19 +0000

    User can upload avatar

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -9,7 +9,7 @@
 		</div>
 		@include('common.error')
 		<div class="panel-body">
-			<form action="{{route('users.update',$user->id)}}" method="POST" accept-charset="UTF-8">
+			<form action="{{route('users.update',$user->id)}}" method="POST" accept-charset="UTF-8" enctype="multipart/form-data">
 				<input type="hidden" name="_method" value="PUT">
 				<input type="hidden" name="_token" value="{{csrf_token()}}">
 				<div class="form-group">
@@ -23,6 +23,14 @@
 				<div class="form-group">
 					<label for="introduction-field">个人简介</label>
 					<textarea name="introduction" id="introduction-field" class="form-control" rows="3">{{old('introduction',$user->introcuntion)}}</textarea>
+				</div>
+				<div class="form-group">
+					<label for="" class="avatar-label">用户头像</label>
+					<input type="file" name="avatar">
+					@if($user->avatar)
+					<br>
+					<img src="{{$user->avatar}}" width="200" class="thumbnail img-responsive">
+					@endif
 				</div>
 				<div class="well well-sm">
 					<button type="submit" class="btn btn-primary">保存</button>
--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -7,7 +7,7 @@
 			<div class="panel-body">
 				<div class="media">
 					<div align="center">
-						<img src="https://fsdhubcdn.phphub.org/uploads/images/201709/20/1/PtDKbASVcz.png?imageView2/1/w/600/h/600" width="300px" height="300px">
+						<img src="{{$user->avatar}}" width="300px" height="300px" class="thumbnail img-responsive">
 					</div>
 					<div class="media-body">
 						<hr>
