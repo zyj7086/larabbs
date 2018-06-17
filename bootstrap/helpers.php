ad644a2014fe39fe27e18e5dac9cdd3281b81dfe
vagrant <vagrant@homestead>
2018-06-17 09:58:16 +0000

    User can create topic

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -4,3 +4,9 @@ function route_class()
 {
     return str_replace('.', '-', Route::currentRouteName());
 }
+
+function make_excerpt($value,$length = 200)
+{
+	$excerpt = trim(preg_replace('/\r\n|\r|\r+/',' ',strip_tags($value)));
+	return str_limit($excerpt,$length);
+}
