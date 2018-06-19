a079f43dd55720f931d371e67be28ea8b7c27594
vagrant <vagrant@homestead>
2018-06-19 11:16:57 +0000

    Admin can manage users

--- app/Models/User.php
+++ app/Models/User.php
@@ -57,4 +57,18 @@ class User extends Authenticatable
         $this->save();
         $this->unreadNotifications->markAsRead();
     }
+    public function setPasswordAttribute($value)
+    {
+        if(strlen($value)!= 60){
+            $value = bcrypt($value);
+        }
+        $this->attributes['password'] = $value;
+    }
+    public function setAvatarAttribute($path)
+    {
+        if(!starts_with($path,'http')){
+            $path=config('app.url')."/uploads/images/avatars/$path";
+        }
+        $this->attributes['avatar']=$path;
+    }
 }
